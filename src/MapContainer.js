2cfa6dc3de265db1ac72ac6a72c0216824770d86
lankolas22 <72575030+lankolas22@users.noreply.github.com>
2021-01-16 20:16:30 +0000

    remove key

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -21,5 +21,5 @@ export class Container extends React.Component {
 }
 
 export default GoogleApiComponent({
-  apiKey: "AIzaSyBVFKqse6hogr0y7ecgJ9SNYxnOiVowENs",
+  apiKey: ftftftftf,
 })(Container);
